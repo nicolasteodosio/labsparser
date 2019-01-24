750cdad43b2ba13ba04b0e1a7428042a719a1cf5
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2019-01-24 12:02:34 -0200

    Tratamento de exceções

--- api/app/api.py
+++ api/app/api.py
@@ -1,34 +1,33 @@
-import json
-
 from flask import Blueprint, jsonify
 
 from api.factory import mongo
-from bson import json_util
+
 api_bp = Blueprint('api', __name__, url_prefix='/api')
-
-
-@api_bp.route('/home', methods=['GET'])
-def home():
-    return 'oi'
 
 
 @api_bp.route('/gamelog/games', methods=['GET'])
 def games():
-    results = mongo.db.gamelog.find()
-    data = []
-    for result in results:
-        for key, value in result.items():
-            if key == '_id':
-                continue
-            else:
-                data.append({key: value})
-    return jsonify(data)
+    try:
+        results = mongo.db.gamelog.find()
+        data = []
+        for result in results:
+            for key, value in result.items():
+                if key == '_id':
+                    continue
+                else:
+                    data.append({key: value})
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'erro': str(e)}), 500
 
 
 @api_bp.route('/gamelog/games/<id>', methods=['GET'])
 def games_one(id):
-    results = mongo.db.gamelog.find_one_or_404({'game_{}'.format(id): {'$exists': True}})
-    for key, value in results.items():
-        if key == 'game_{}'.format(id):
-            return jsonify({key: value})
-    return 404
+    try:
+        results = mongo.db.gamelog.find_one_or_404({'game_{}'.format(id): {'$exists': True}})
+        for key, value in results.items():
+            if key == 'game_{}'.format(id):
+                return jsonify({key: value})
+        return jsonify({'erro': '404'}), 404
+    except Exception as e:
+        return jsonify({'erro': str(e)}), 500
