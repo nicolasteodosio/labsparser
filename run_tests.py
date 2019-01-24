ec2d1ec532e2768d184d1dec554d23fe72035113
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2019-01-24 12:07:56 -0200

    Add requisitos de teste do flask e runner geral

---
+++ run_tests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+import pytest
+
+
+os.environ['TEST'] = '1'
+args = ['-rsxX', '--tb=native'] + sys.argv[1:]
+
+sys.exit(pytest.main(args + ['api/tests/', 'parser/tests/']))
