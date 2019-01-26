c17e2dc6749c3152f1f96a6260d2544114021ed8
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2019-01-26 17:53:49 -0200

    Modificando database, usando envvar

--- api/factory.py
+++ api/factory.py
@@ -1,7 +1,11 @@
+import os
+
 from flask import Flask
 from flask_pymongo import PyMongo
 
 mongo = PyMongo()
+
+DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
 
 
 def create_app(app_name):
@@ -11,11 +15,11 @@ def create_app(app_name):
     return app
 
 
-def configure_blueprints(app):
+def configure_blueprints(application):
     from api.app.api import api_bp
-    app.register_blueprint(api_bp)
+    application.register_blueprint(api_bp)
 
 
 def configure_mongo(app):
-    app.config["MONGO_URI"] = "mongodb://root:example@localhost:27017/gamelog?authSource=admin"
+    app.config["MONGO_URI"] = "mongodb://root:example@{}:27017/gamelog?authSource=admin".format(DATABASE_HOST)
     mongo.init_app(app)
